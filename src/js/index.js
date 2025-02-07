921ab9a5bef7b639ab3f82aa115f728031052707
Genezis <ucmessina@alumnostrazos.net>
2025-02-07 21:52:40 +0100

    +first Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,48 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
-
 import '../scss/styles.scss';
+import { ProyectElements } from './project';
+const menuElement = document.getElementById('menu');
+const hamburgerElement = document.getElementById('hamburger');
 
-button.addEventListener('click', () => {
-  console.log('BUTTON CLICKED');
+const toggleMenu = () => {
+  menuElement.classList.toggle('menu--active');
+
+  const isMenuActive = menuElement.classList.contains('menu--active');
+  hamburgerButton.src = isMenuActive ? './assets/images/hamburguer.svg' : './assets/images/hamburguer.svg';
+};
+
+hamburgerElement.addEventListener('click', function (e) {
+  e.preventDefault();
+  this.classList.toggle('active-menu');
 });
+const proyectContainer = document.getElementById('proyect');
+const fragment = document.createDocumentFragment();
+
+ProyectElements.forEach(element => {
+  const divElement = document.createElement('div');
+  divElement.classList.add('project__box');
+
+  const flexContainer = document.createElement('div');
+  flexContainer.classList.add('project__elements');
+
+  const textElement = document.createElement('h2');
+  textElement.classList.add('project__text');
+  textElement.textContent = element.text;
+  flexContainer.appendChild(textElement);
+
+  const buttonElement = document.createElement('a');
+  buttonElement.href = '#';
+
+  const buttonImage = document.createElement('img');
+  buttonImage.src = element.imgButton;
+  buttonImage.alt = 'Button';
+
+  buttonElement.classList.add('project__button');
+
+  buttonElement.appendChild(buttonImage);
+  flexContainer.appendChild(buttonElement);
+  divElement.appendChild(flexContainer);
+
+  fragment.appendChild(divElement);
+});
+
+proyectContainer.insertBefore(fragment, proyectContainer.firstChild);
---
+++ src/js/project.js
@@ -0,0 +1,35 @@
+export const ProyectElements = [
+  {
+    bgImage: 'url("./assets/images/Card1.jpg")',
+    bgImageTablet: 'url("./assets/images/Card2.jpg")',
+    bgImageLaptop: 'url("./assets/images/Card3.jpg")',
+    bgImageDesktop: 'url("./assets/images/Card4.jpg")',
+    text: 'New Project',
+    imgButton: './assets/images/Arrow.svg'
+  },
+  {
+    bgImage: 'url("./assets/images/Card1.jpg")',
+    bgImageTablet: 'url("./assets/images/Card1.jpg")',
+    bgImageLaptop: 'url("./assets/images/Card1.jpg")',
+    bgImageDesktop: 'url("./assets/images/Card1.jpg")',
+    text: 'New Project',
+    imgButton: './assets/images/Arrow.svg'
+  },
+  {
+    bgImage: 'url("./assets/images/Card1.jpg")',
+    bgImageTablet: 'url("./assets/images/Card1.jpg")',
+    bgImageLaptop: 'url("./assets/images/Card1.jpg")',
+    bgImageDesktop: 'url("./assets/images/Card1.jpg")',
+    text: 'New Project',
+    imgButton: './assets/images/Arrow.svg'
+  },
+
+  {
+    bgImage: 'url("./assets/images/Card1.jpg")',
+    bgImageTablet: 'url("./assets/images/Card1.jpg")',
+    bgImageLaptop: 'url("./assets/images/Card1.jpg")',
+    bgImageDesktop: 'url("./assets/images/Card1.jpg")',
+    text: 'New Project',
+    imgButton: './assets/images/Arrow.svg'
+  }
+];
